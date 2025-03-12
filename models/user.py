50f1b90de9222bfcec41791dc7d893b56f9ed095
andikavisionplus <andika.septalonika@visionplus.id>
2025-03-12 17:26:45 +0700

    add user models

---
+++ models/user.py
@@ -0,0 +1,11 @@
+list_user_db = {"users": [
+    { 
+        "id": 1,
+        "created_at": "2025-09-08 15:00:00",
+        "email": "andika@mail.com",
+        "first_name": "andika",
+        "last_name": "Septalonika",
+        "full_name": "andika Septalonika",
+        "password": "terserah gue"
+    }
+]}
