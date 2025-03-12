8b1c7999866d6f0870077e082383bc949b977c76
andikavisionplus <andika.septalonika@visionplus.id>
2025-03-12 18:12:17 +0700

    add update and delete user routers

--- views/user.py
+++ views/user.py
@@ -61,3 +61,37 @@ def create_user():
         return jsonify({"data": formatted_user_list, "message": f"User {formatted_user_list["full_name"]} created", "success": True, "status": 201})
     except:
         return jsonify({"data": [], "message": "user failed to create", "success": False, "status": 500})
+        
+def delete_user(user_id):
+    user = next((user for user in list_user_db["users"] if user["id"] == user_id), None)
+    if user is None:
+        return jsonify({"data": [], "message": "User not found", "success": False, "status": 404}), 404
+    list_user_db["users"].remove(user)
+    list_user = list_user_db["users"].copy()
+    formatted_user_list = []
+    for user in list_user:
+        formatted_user_list.append(
+            {
+                "id": user["id"],
+                "email": user["email"],
+                "full_name": user["full_name"]
+            }
+        )
+    return jsonify({"data": formatted_user_list, "message": f"User {user["full_name"]} deleted successfully", "success": True, "status": 202})
+
+def update_user(user_id):
+    update_data = request.json
+    email = update_data.get("email")
+    password = update_data.get("password")
+    first_name = update_data.get("first_name")
+    last_name = update_data.get("last_name")
+    full_name = f"{first_name} {last_name}"
+    user = next((user for user in list_user_db["users"] if user["id"] == user_id), None)
+    if user is None:
+        return jsonify({"data": [], "message": "User not found", "success": False, "status": 404}), 404
+    user["email"] = email
+    user["password"] = password
+    user["first_name"] = first_name
+    user["last_name"] = last_name
+    user["full_name"] = full_name
+    return jsonify({"data": user, "message": f"User {full_name} updated successfully", "success": True, "status": 200})
